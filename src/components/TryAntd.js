74f36364ba5953bf8fa560f388049585397f7277
Plengpai Ratnajarn <pinplengpai@gmail.com>
2020-04-03 15:59:32 +0700

    try to undertstand antd and how it works

---
+++ src/components/TryAntd.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { drawer } from 'styled';
+import styled, { css } from 'styled-components';
+import { SurveyCard } from '../styles';
+
+
+const CardExample = styled(Card)`
+    background= "red";
+    border-radius: 10px;
+
+    .ant-card-body {
+        padding: 0px !important;
+    } 
+`
+
+function CardEx() {
+    <SurveyCard
+    
+    >
+}
---
+++ src/styles/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Card from 'antd/lib/card';
+
+
+
+export const TextCardTitle = styled.span`
+    font-size: 16px;
+    font-weight: 400;
+    margin: 0 auto;
+`
+
+export const SurveyCard = styled(Card)`
+    border-radius : 20px;
+    padding: 10px;
+`
