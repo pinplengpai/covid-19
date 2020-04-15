53b6c00887d845219ed84d1f413318f0f35841d2
minnyww <minrrverse@gmail.com>
2020-04-15 16:14:50 +0700

    [UPdate] last

--- src/components/CaseStat.js
+++ src/components/CaseStat.js
@@ -41,30 +41,6 @@ function CaseStat() {
     }
   })
   const genderCount = [{ "male": maleCount, "female": femaleCount, "nonbinary": nullCount }];
-  // const genderCount = [{ "male": 12345, "female": 4021, "nonbinary": 45712 }]
-  const test = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-  ];
   console.log(genderCount)
   // console.log(genderCount)
   // let number = ata && data.records && data.records.map(item =>{
@@ -116,21 +92,19 @@ function CaseStat() {
 
   return (
     <>
-      <>
-        {isLoading && <div> Loading ... </div>}
-        {!isLoading && genderCount &&
-          <BarChart width={730} height={250} data={genderCount}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="gender" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="male" fill="#8884d8" />
-            <Bar dataKey="female" fill="#82ca9d" />
-            <Bar dataKey="nonbinary" fill="#82ca9d" />
-          </BarChart>
-        }
-      </>
+      {isLoading && <div> Loading ... </div>}
+      {!isLoading && genderCount &&
+        <BarChart width={730} height={250} data={genderCount}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="gender" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="male" fill="#8884d8" />
+          <Bar dataKey="female" fill="#82ca9d" />
+          <Bar dataKey="nonbinary" fill="#82ca9d" />
+        </BarChart>
+      }
     </>
   )
 }
